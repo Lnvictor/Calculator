dd36dd76a5a8652ad612c6ba5a90242a9175ec42
Victor Pereira <Lnvictor@github.com>
2020-03-29 19:57:36 -0300

    Old Project

--- src/tests/TestKeyEqualPressed.java
+++ src/tests/TestKeyEqualPressed.java
@@ -6,7 +6,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class TestEnterPressed {
+public class TestKeyEqualPressed {
     private MainApp mainTest;
 
     @Before
